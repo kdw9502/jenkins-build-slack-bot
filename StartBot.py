89e0fce4980f30c121470ea581de7c15c0a53519
kdw9502 <kdw9502@gmail.com>
2020-02-26 18:26:46 +0900

    reboot after exception

--- StartBot.py
+++ StartBot.py
@@ -1,14 +1,16 @@
+import argparse
 import asyncio
+import traceback
 
 from slacker import Slacker
-import argparse
+
 from JenkinsBuildBot import JenkinsBuildBot
 
 
 async def main():
     parser = argparse.ArgumentParser(description="Simple jenkins build invoke bot")
 
-    parser.add_argument('bot_token',help='Bot User OAuth Access Token')
+    parser.add_argument('bot_token', help='Bot User OAuth Access Token')
     parser.add_argument('user_token', help='OAuth Access Token')
     parser.add_argument('jenkins_url', help='jenkins server url')
 
@@ -18,7 +20,12 @@ async def main():
     user_slack = Slacker(args.user_token)
 
     bot = JenkinsBuildBot(bot_slack, user_slack, args.jenkins_url)
-    await bot._listen()
+    while True:
+        try:
+            await bot._listen()
+        except:
+            traceback.print_exc()
+            await asyncio.sleep(60)
 
 
 if __name__ == '__main__':
